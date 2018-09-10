7d8f4a5e742da4fbdd93325545991c4aebe074ee
Youngjun Na <ruminoreticulum@gmail.com>
2018-09-10 11:52:28 +0900

    add normalization

---
+++ R/back_normalization.R
@@ -0,0 +1,17 @@
+#' back_normalization
+#'
+#' A function for back_normalization.
+#' @keywords deep learning goat methane
+#' @export
+#' @examples
+#' neuralnet::compute(model1,example1)
+#' back_normalization(data=example1$net.result)
+
+back_normalization <- function(data) {
+  back_normalize <- function(x) {
+    return(x*(max(minmax$CH4)-min(minmax$CH4))+(min(minmax$CH4)))
+  }
+  data <- data.frame(CH4=data)
+  back_nor <- as.data.frame(lapply(data,back_normalize))
+  return(back_nor)
+}
